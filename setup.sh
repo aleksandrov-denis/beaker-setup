f44911a66bdd71ce46ae50a6ef50b1281de78081
Denis Aleksandrov <denisuzh@gmail.com>
2023-03-30 15:44:42 -0400

    Adds cloning of the KDLP_assignments repo
    Signed-off-by: Denis Aleksandrov <denisuzh@gmail.com>

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ dnf install -y git bc tar ncurses-devel ccache zstd kernel-devel
 
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 
+git clone https://github.com/underground-software/KDLP_assignments.git
+
 ### IN .config ###
 # Mark the .pem line as blank
 # Disable the SIG_ALL and CONFIG_DEBUG_INFO_BTF lines
